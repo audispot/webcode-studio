a22a698474d3196f75f4b4508417e361446f0052
audispot <ismaelkea818@gmail.com>
2026-09-19 13:56:38 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBCMr7-uyBINkV7YU4yNoASziiUQZ2iO2o",
+  authDomain: "webcode-studio1.firebaseapp.com",
+  projectId: "webcode-studio1",
+  storageBucket: "webcode-studio1.firebasestorage.app",
+  messagingSenderId: "181209063296",
+  appId: "1:181209063296:web:bc8d39956538873a496b65",
+  measurementId: "G-H63F1Q1F0P"
+};
